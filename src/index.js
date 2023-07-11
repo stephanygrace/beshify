ea5cee6c45eedc9d9c01d0ee80f5563158ad9282
stephanygrace <stephanytayong73@gmail.com>
2023-07-11 15:51:59 +0800

    modify routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../src/App.css";
+import Copy from "../src/copy.png";
+import Github from "../src/github.png";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [inputText, setInputText] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === " ") {
+      setInputText((prevText) => prevText + " 🤸 ");
+    }
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(inputText);
+    toast.success("Text copied!");
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="title"> 🤸 Beshify 🤸</h1>
+      </div>
+      <div className="body-beshify">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          className="input"
+        />
+        <button onClick={handleCopyClick}>
+          <img src={Copy} alt="" className="copy" />
+        </button>
+      </div>
+      <div className="footer">
+        <a
+          href="https://github.com/stephanygrace"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Github} alt="" />
+        </a>
+        <span> Stephany Grace Tayong </span>
+      </div>
+      <ToastContainer autoClose={1000} />
+    </div>
+  );
+};
+
+export default App;
